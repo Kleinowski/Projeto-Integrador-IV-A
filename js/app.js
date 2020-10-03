c7ca1dc89eca5e738903c78eede7f2e3dab80ae1
Willian Kleinowski <wkleinowski@hotmail.com>
2020-10-02 22:17:30 -0300

    Adicionada parte final do trabalho

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ var swiper = new Swiper('.swiper-container', { // Criar novo slide
       prevEl: '.swiper-button-prev',
     },
 });
+
+// Variáveis do carrinho e dos botões de compra
+let carrinho = document.querySelector("#carrinho");
+let produtoBtn = document.querySelectorAll(".produto-btn");
+
+// Variável para contar o número de itens no carrinho
+let itensCarrinho = 0;
+
+// Loop para atribuir o eventListener e a função para todos botões
+for (let i = 0; i < produtoBtn.length; i++) {
+  produtoBtn[i].addEventListener("click", function(e) {
+    // Utiliza evento para prevenir o link do seu comportamento padrão
+    e.preventDefault();
+
+    // Soma itens no carrinho a cada clique em um botão e atualiza o número de itens no menu
+    itensCarrinho++;
+    carrinho.innerHTML = `Carrinho (${itensCarrinho}) <i class="fas fa-shopping-cart"></i>`;
+  });
+}
